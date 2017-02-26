aae37fe0880e64c9e655b5f9fba997ffbad473b8
petercanmakit <zhangjj2007@hotmail.com>
2017-02-25 21:39:01 -0500

    Thead

--- UdpChat.java
+++ UdpChat.java
@@ -2,7 +2,7 @@ import java.net.*;
 import java.util.*;
 
 class UdpChatServer{
-	Hashtable<String,ArrayList<String>> clients = new HashMap();
+	Hashtable<String,ArrayList<String>> clients = new Hashtable<String,ArrayList<String>>();
 	// <nick-name,[ip,port,status]>
 	int port;
 	DatagramSocket ds;
@@ -17,34 +17,42 @@ class UdpChatServer{
 		ds.send(dp);
 	}
 
-	public void broadcast(){
+	public void broadcast() throws Exception{
 		if(clients.isEmpty()){
 			// do nothing
 		}
 		else{
 			Enumeration users = clients.keys();
 			while(users.hasMoreElements()){
-				String nick_name = users.nextElement();
+				String nick_name = new String(String.valueOf(users.nextElement()));
+				String user_status = clients.get(nick_name).get(2);
+				if(user_status.equals("off"))
+					continue;
+				//System.out.println(nick_name);
 				String user_ip = clients.get(nick_name).get(0);
-				String user_port = clients.get(nick_name).get(1);
+				//System.out.println(user_ip);
+				int user_port = Integer.valueOf(clients.get(nick_name).get(1));
+				//System.out.println(user_port);
+				//System.out.println(clients.toString());
 				this.send(clients.toString(),user_ip,user_port);
 			}
 		}
 	}
 
-	public void register(String info){
-		String[] in = info.split('*');
-		System.out.println(in[0]);
-		System.out.println(in[1]);
-		System.out.println(in[2]);
+	public void register(String info) throws Exception {
+		String[] in = info.split("#");
+		//System.out.println(in[0]);
+		//System.out.println(in[1]);
+		//System.out.println(in[2]);
 		String nick_name = in[0];
 		String clnt_ip = in[1];
 		String clnt_port = in[2];
+		String clnt_status = "on";
 		if(clients.containsKey(nick_name)){
 			// TODO send nak
 		}
 		else{
-			clients.put(nick_name,new ArrayList(Arrays.asList(clnt_ip,clnt_port)));
+			clients.put(nick_name,new ArrayList(Arrays.asList(clnt_ip,clnt_port,clnt_status)));
 			// TODO send ack
 			this.broadcast();
 		}
@@ -53,9 +61,11 @@ class UdpChatServer{
 	public void recv_register() throws Exception {
 		byte[] buf = new byte[1024];
 	    DatagramPacket dp = new DatagramPacket(buf, 1024);
+		//System.out.println("in the register()");
 	    ds.receive(dp);
 	    String info = new String(dp.getData(), 0, dp.getLength());
-		System.out.println(info);
+		//System.out.println(info);
+		this.register(info);
 	}
 
 	public  void destroy(){
@@ -84,10 +94,18 @@ class UdpChatServer{
 
 class UdpChatClient{
 
+	Hashtable<String,ArrayList<String>> clients = new Hashtable<String,ArrayList<String>>();
+
+	String nick_name;
 	int port;
+	String server_ip;
+	int server_port;
 	DatagramSocket ds;
-	UdpChatClient(int portNumber) throws Exception{
+	UdpChatClient(int portNumber, String serverIP, int serverPort, String nname) throws Exception{
+		server_ip = serverIP;
 		port = portNumber;
+		server_port = serverPort;
+		nick_name = nname;
 		ds = new DatagramSocket(port);
 	}
 
@@ -95,12 +113,42 @@ class UdpChatClient{
 		ds.close();
 	}
 
-	public String recv() throws Exception{
+	public void updateClients (String str){
+		// formmat: {jiajun=[160.39.141.140, 8082], peter=[160.39.141.140, 8081], haha=[160.39.141.140, 8083]}
+		clients.clear();
+		str = str.replace("{","");
+		str = str.replace("}","");
+		str = str.replaceAll("\\s","");
+		//String [] strs = str.split("=[|],");
+		//System.out.println(str);
+		String [] strs = str.split("\\],|=\\[");
+
+		for(int i =0;i<strs.length;i=i+2){
+			String nick_name = strs[i];
+			String tmp = strs[i+1];
+			String[] ipNport = tmp.split(",");
+			clients.put(nick_name,new ArrayList<String>(Arrays.asList(ipNport)));
+		}
+
+	}
+
+	public void recv() throws Exception{
 		byte[] buf = new byte[1024];
 	    DatagramPacket dp = new DatagramPacket(buf, 1024);
 	    ds.receive(dp);
-	    String str = new String(dp.getData(), 0, dp.getLength());
-	    return str;
+		System.out.println("Receiving!");
+		String str = new String(dp.getData(), 0, dp.getLength());
+		if(dp.getAddress().equals(server_ip) && dp.getPort()==this.server_port){
+			// this packet is from server
+			updateClients(str);
+			System.out.println(">>> [Client table updated.]");
+		}
+		else{
+			// this packet is from other client
+			// TODO send ack
+			System.out.println(str);
+		}
+
 	}
 
 	public  void send(String content, String ip_str, int port) throws Exception{
@@ -128,6 +176,68 @@ class UdpChatClient{
 	 }
 
 }
+
+// sender and receiver threads
+class sender implements Runnable {
+	UdpChatClient client;
+	int mutex;
+	public sender(UdpChatClient cc, int mm){
+		this.client = cc;
+		this.mutex = mm;
+	}
+
+	@Override
+	public void run(){
+		while(true){
+			Scanner sc = new Scanner(System.in);
+			String line = sc.nextLine();
+			System.out.println(line);
+			String[] usr_in = line.split("\\s+",3);
+			for(int i =0;i<usr_in.length;i++){
+				System.out.println(usr_in[i]);
+			}
+			if(usr_in[0].equals("send")){
+				String name = new String(usr_in[1]);
+				String message = new String(usr_in[2]);
+				System.out.println("Sending");
+				System.out.println(client.clients.toString());
+				try{
+					client.send(client.nick_name+":  "+message,client.clients.get(name).get(0),
+								Integer.valueOf(client.clients.get(name).get(1)));
+				}
+				catch(Exception ex){}
+			}
+			else{
+				System.out.println("Usage: send <name> <message>");
+			}
+			System.out.print("sender>>> ");
+		}
+	}
+}
+
+class receiver implements Runnable {
+	UdpChatClient client;
+	int mutex;
+	public receiver(UdpChatClient cc, int mm){
+		this.client = cc;
+		this.mutex = mm;
+	}
+
+	@Override
+	public void run(){
+		while(true){
+			try{
+				client.recv();
+			}
+			catch(Exception ex){
+				System.out.println(String.valueOf(ex));
+			}
+
+			System.out.print("receiver>>> ");
+		}
+	}
+}
+
 
 public class UdpChat{
 
@@ -173,20 +283,23 @@ public class UdpChat{
 			  System.out.println("nick-name is "+nick_name+"\nserver-ip is "+server_ip+
 			  					"\nserver-port is "+server_port+"\nclient-port is "+client_port);
 			  // start client
-			  UdpChatClient client = new UdpChatClient(client_port);
-			  client.send(nick_name+"*"+client.getIP()+"*"+client_port,server_ip,server_port);
+			  UdpChatClient client = new UdpChatClient(client_port,server_ip,server_port,nick_name);
+			  client.send(nick_name+"#"+client.getIP()+"#"+client_port,server_ip,server_port);
 			  //System.out.println(client.recv());
 			  System.out.print(">>> [Welcome. You are registered.]\n");
-			  while(true){
-				  System.out.print(">>>");
-			  }
+
+			  System.out.print(">>> ");
+			  int mutex = 1;
+			  Thread t_sender = new Thread(new sender(client,mutex));
+			  Thread t_receiver = new Thread(new receiver(client,mutex));
+			  t_sender.start();
+			  t_receiver.start();
+
+			  t_sender.join();
+			  t_receiver.join();
 			  client.destroy();
 		  }
 	  }
-	  // UdpChat -s <port> : Initiate the server process
-
-	  // UdpChat -c <nick-name> <server-ip> <server-port> <client-port> : Initiate the client
-
 
     }
 }
